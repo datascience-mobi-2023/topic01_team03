ba0d92b3f709ffed0fa31704036e6b469d8456e0
FrankaBegall <franka@begall.de>
2023-06-27 09:36:31 +0200

    blabla

---
+++ Anwendung/my_functions/histogram.py
@@ -0,0 +1,8 @@
+import numpy as np
+def histogrammstretching(img,a,b):
+    c = min(img.ravel())
+    d = max(img.ravel())
+    img_copy = img.copy()
+    for p in np.ndindex(img_copy.shape):
+        img_copy[p] = (img[p]-c) * ((b-a)/(d-c))+a
+    return img_copy
